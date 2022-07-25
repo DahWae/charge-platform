5f9d39a1236008821b0f61849954f3e405d68281
DahWae <40575026h@gapps.ntnu.edu.tw>
2022-07-25 16:40:36 +0800

    ready for moving test

--- server/ClientView/vite.config.js
+++ server/ClientView/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     host: '0.0.0.0'
   },
   define: {
-    serverUrl: JSON.stringify('http://192.168.100.5:8001'),
-    robotUrl: JSON.stringify('http://192.168.100.5:8000')
+    serverUrl: JSON.stringify('http://192.168.100.3:8001'),
+    robotUrl: JSON.stringify('http://192.168.100.3:8000')
   }
 })
